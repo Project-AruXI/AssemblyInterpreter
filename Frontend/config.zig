a1ef6e25f8d856cbb050d4bdc7fcc284ee152e55
Jorge <jorgegarcia_tx@outlook.com>
2026-05-29 22:05:51 -0500

    Custom formatting for CliConfig

--- Frontend/config.zig
+++ Frontend/config.zig
@@ -7,8 +7,10 @@ pub const CliConfig = struct {
 	stackSize:u32,
 	heapSize:u32,
 	enableAVExt:bool,
-	filePath:?[]const u8,
 	allocator:std.mem.Allocator,
+
+	// File-based interpreter only
+	filePath:?[]const u8,
 
 	// REPL only
 	allowSystem:bool,
@@ -24,4 +26,49 @@ pub const CliConfig = struct {
 			.allocator = allocator,
 		};
 	}
+
+	pub fn format(self: *const CliConfig, writer: *std.Io.Writer) !void {
+		try writer.writeAll("CliConfig{\n");
+		inline for (@typeInfo(CliConfig).@"struct".fields) |field| {
+			_print: {
+				// If the field is allocator, skip it
+				// If the field type is a string, print as a string
+				if (std.mem.eql(u8, field.name, "allocator")) {
+					break :_print;
+				}
+				const value = @field(self, field.name);
+
+				if (comptime std.mem.eql(u8, field.name, "filePath")) {
+					try writer.print("{s}: {?s}\n", .{field.name, value});
+				} else {
+					try writer.print("{s}: {any}\n", .{field.name, value});
+				}
+			}
+		}
+		try writer.writeAll("}\n");
+	}
 };
+
+test "formatNoPath" {
+	const cfg = CliConfig.init(65536, 4096, 16384, false, true, null, std.testing.allocator);
+	
+	const cfgString = try std.fmt.allocPrint(std.testing.allocator, "{f}", .{cfg});
+	defer std.testing.allocator.free(cfgString);
+	
+	try std.testing.expectEqualStrings(
+		"CliConfig{\nmemSize: 65536\nstackSize: 4096\nheapSize: 16384\nenableAVExt: false\nfilePath: null\nallowSystem: true\n}\n", 
+		cfgString
+	);
+}
+
+test "formatPath" {
+	const cfg = CliConfig.init(4096 * 5, 2048, 2048, true, false, "./samples/org.as", std.testing.allocator);
+
+	const cfgString = try std.fmt.allocPrint(std.testing.allocator, "{f}", .{cfg});
+	defer std.testing.allocator.free(cfgString);
+
+	try std.testing.expectEqualStrings(
+		"CliConfig{\nmemSize: 20480\nstackSize: 2048\nheapSize: 2048\nenableAVExt: true\nfilePath: ./samples/org.as\nallowSystem: false\n}\n",
+		cfgString
+	);
+}
